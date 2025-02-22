c9fa6394e8dd2bb9564d7b2d598d478c7deea5ec
Parth200611 <parthdahapute7@gmail.com>
2025-02-22 23:55:35 +0530

    commit

--- app/src/main/java/com/mountreachsolution/sharebite/urls.java
+++ app/src/main/java/com/mountreachsolution/sharebite/urls.java
@@ -5,4 +5,5 @@ public class urls {
     public static String userregister=address+"registerdata.php";
     public static String certificateimage=address+"certificateimage.php";
     public static String login=address+"login.php";
+    public static String Profildat=address+"profildat.php";public static String profilimage=address+"profilimage.php";
 }
